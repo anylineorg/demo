49e37c3c85173e5ae3ba5487a9ed7ce627582189
xiayousong <xiayousong@outlook.com>
2020-01-17 09:31:53 +0800

    anyboot-non-web-mysql

--- anyboot-security-oauth2/anyboot-security-oauth2-server/src/main/java/org/anyboot/security/oauth2/config/AuthorizationServerConfiguration.java
+++ anyboot-security-oauth2/anyboot-security-oauth2-server/src/main/java/org/anyboot/security/oauth2/config/AuthorizationServerConfiguration.java
@@ -1,14 +1,9 @@
 package org.anyboot.security.oauth2.config;
 
-import org.anyline.entity.DataRow;
-import org.anyline.service.AnylineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,7 +14,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
-
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
